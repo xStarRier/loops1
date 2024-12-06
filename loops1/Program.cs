418ff5da0b86037a731e3050d880f9ac847341ce
xStarRier <stephanniequynh@gmail.com>
2024-12-06 14:36:41 +0100

    Add project files.

---
+++ loops1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] str = new string[] { "Terje er kul", "Terje er kul", "Terje er kul", "Terje er kul", "Terje er kul" };
+
+
+for (var i = 0; i < str.Length; i++) 
+    {
+        Console.WriteLine(str[i]);
+    }
